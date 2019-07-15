23b588942d172d521dd72ba16435e11b052c8592
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2019-07-15 13:03:35 -0600

    style changes
    use of
    position: absolute;

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 'use strict';
 
 
-
 // Event Listeners
 document.getElementById('item-btn').addEventListener('click', getItem);
 document.getElementById('reset-btn').addEventListener('click', reset);
@@ -11,14 +10,12 @@ document.getElementById('reset-btn').addEventListener('click', reset);
 let kart = 1
 
 // Make table of karts
-function displayKarts() {
-    for (let num = 1; num < 4 + 1; num++) {
-        for (let col = 1; col < 3 + 1; col++) {
-            document.getElementById('karts').innerHTML += "<div class='hover-kart' id='kart" + kart + "'><img src='images/kart" + kart++ + ".png'><br>" + (kart - 1) + "</div>";
-        }
+for (let num = 1; num < 4 + 1; num++) {
+    for (let col = 1; col < 3 + 1; col++) {
+        document.getElementById('karts').innerHTML += "<div class='hover-kart' id='kart" + kart + "'><img src='images/kart" + kart++ + ".png'><br>" + (kart - 1) + "</div>";
     }
 }
-displayKarts(); // Display karts/positions
+
 
 // Event Function
 function getItem() {
@@ -36,6 +33,9 @@ function getItem() {
 
     if (pos >= 1 && pos <= 6) { // Position 1-6
         console.log('positons1-6');
+
+        document.getElementById('rare-item').classList.add('hidden');
+
         if (rand < 0.45) { // 45%
             result.innerHTML += '<img src="images/banana.png">';
         } else if (rand < 0.80) { // 35%
@@ -46,10 +46,14 @@ function getItem() {
             result.innerHTML += '<img src="images/gold_mush.png">';
         } else { // 1%
             result.innerHTML += '<img src="images/bullet.png">';
+            document.getElementById('rare-item').classList.remove('hidden');
         }
 
     } else if (pos >= 7 && pos <= 12) { // Position 7-12
         console.log('positons7-12');
+
+        document.getElementById('rare-item').classList.add('hidden');
+
         if (rand < 0.30) { // 30%
             result.innerHTML += '<img src="images/bullet.png">';
         } else if (rand < 0.65) { // 35%
@@ -58,14 +62,21 @@ function getItem() {
             result.innerHTML += '<img src="images/star.png">';
         } else if (rand < 0.95) { // 5%
             result.innerHTML += '<img src="images/green_shell.png">';
+            document.getElementById('rare-item').classList.remove('hidden');
+
         } else { // 5%
             result.innerHTML += '<img src="images/banana.png">';
+            document.getElementById('rare-item').classList.remove('hidden');
+
         }
 
     } else {
         alert('Error! Invalid number. \nChoose a number between 1 and 12.');
     }
 }
+
 function reset() {
     document.getElementById('result').innerHTML = '';
+    document.getElementById('rare-item').classList.add('hidden');
+
 }
